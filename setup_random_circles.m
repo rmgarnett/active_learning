b9aa46263ba1d42922486bd46d928fedbe2b7db5
rmgarnett <romanempire@gmail.com>
2011-09-15 23:43:35 -0400

    random circle test script

---
+++ setup_random_circles.m
@@ -0,0 +1,28 @@
+num_observations = 500;
+dimension = 2; %randi([2 5], 1);
+
+num_circles = 5; %randi([1 5], 1);
+
+flip_percentage = 0;
+
+num_evaluations = 10;
+num_f_samples = 1000;
+
+data = rand(num_observations, dimension);
+
+centers = rand(num_circles, dimension);
+radii = rand(num_circles) / 2;
+responses = false(num_observations, 1);
+for i = 1:num_circles
+  responses = responses | ...
+      (sum((data - repmat(centers(i, :), num_observations, 1)).^2, 2) < ...
+       radii(i).^2);
+end
+
+actual_proportion = mean(responses);
+responses = 2 * responses - 1;
+
+flip = rand(size(responses)) < flip_percentage;
+responses(flip) = -responses(flip);
+
+disp(['dimension: ' num2str(dimension) ', circles: ' num2str(num_circles)]);
