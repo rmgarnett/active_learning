9fb43d8771f738e0039354d4063414578838639c
rmgarnett <romanempire@gmail.com>
2011-09-30 01:27:23 -0400

    beefing up circle experiment

--- discrete/experiments/random_circle_experiment.m
+++ discrete/experiments/random_circle_experiment.m
@@ -1,33 +1,39 @@
 initialize;
 
+num_experiments = 200;
+
 num_observations = 250;
-dimension = 2; %randi([2 5], 1);
-
-num_circles = randi([1 10], 1);
-
-flip_percentage = 0;
+dimension = 2;
+flip_percentage = 0.05;
 
 num_evaluations = 10;
 num_f_samples = 1000;
 
-data = rand(num_observations, dimension);
+for count = 1:num_experiments
+  disp(['experiment ' num2str(count) ' of ' num2str(num_experiments)]);
 
-centers = rand(num_circles, dimension);
-radii = rand(num_circles) / 2;
-responses = false(num_observations, 1);
-for i = 1:num_circles
-  responses = responses | ...
-      (sum((data - repmat(centers(i, :), num_observations, 1)).^2, 2) < ...
-       radii(i).^2);
+  data = rand(num_observations, dimension);
+
+  num_circles = randi([1 10], 1);
+  centers = rand(num_circles, dimension);
+  radii = rand(num_circles) / 2;
+  responses = false(num_observations, 1);
+  for i = 1:num_circles
+    responses = responses | ...
+                (sum((data - repmat(centers(i, :), num_observations, 1)).^2, 2) < ...
+                 radii(i).^2);
+  end
+
+  actual_proportion = mean(responses);
+  responses = 2 * responses - 1;
+  
+  flip = rand(size(responses)) < flip_percentage;
+  responses(flip) = -responses(flip);
+  
+  disp(['dimension: ' num2str(dimension) ', circles: ' ...
+        num2str(num_circles)]);
+  
+  active_estimation_discrete;
+
+  save(['results/2/' num2str(count)]);
 end
-
-actual_proportion = mean(responses);
-responses = 2 * responses - 1;
-
-flip = rand(size(responses)) < flip_percentage;
-responses(flip) = -responses(flip);
-
-disp(['dimension: ' num2str(dimension) ', circles: ' ...
-      num2str(num_circles)]);
-
-active_estimation_discrete;
