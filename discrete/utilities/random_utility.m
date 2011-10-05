c8640b6fbe6850a4b86b6b5cdbf9a9cf404a57e9
rmgarnett <romanempire@gmail.com>
2011-10-05 16:41:34 -0400

    namespace unmangling, formatting

--- discrete/utilities/random_utility.m
+++
@@ -1,6 +0,0 @@
-function utilities = random_utility(data, responses, in_train)
-
-  num_test = nnz(~in_train);
-  utilities = rand(num_test, 1);
-
-end
--- discrete/utilities/uncertainty_utility.m
+++
@@ -1,7 +0,0 @@
-function utilities = uncertainty_utility(data, responses, in_train, ...
-          probability_function)
-
-  probabilities = probability_function(data, responses, in_train);
-  utilities = -abs(probabilities - (1 / 2)); 
-
-end
