d263ef0967ec97e42cfd7d34f4e7bfd2d6c860ef
rmgarnett <romanempire@gmail.com>
2011-10-12 06:39:54 -0400

    name change

--- general/utilities/meta/restricted_search_utility_wrapper.m
+++ general/utilities/meta/restricted_search_utility_wrapper.m
@@ -1,12 +1,10 @@
-function utilities = partial_search_utility_wrapper(data, responses, ...
-        test, utility_function, num_test_points)
+function utilities = restricted_search_utility_wrapper(data, responses, ...
+          test, selection_function, utility_function)
 
   num_test = size(test, 1);
-
   utilities = -Inf(num_test, 1);
 
-  r = randperm(num_test);
-  test_ind = r(1:num_test_points);
+  test_ind = selection_function(data, responses, test);
   
   utilities(test_ind) = ...
       utility_function(data, responses, test(test_ind, :));
