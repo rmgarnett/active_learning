3634a68964deeee27e1358a19b4d051eaab29147
rmgarnett <romanempire@gmail.com>
2011-10-12 06:39:44 -0400

    formatting

--- general/other/moment_matched_beta.m
+++ general/other/moment_matched_beta.m
@@ -2,4 +2,5 @@ function [alpha beta] = moment_matched_beta(mean, variance)
 
   alpha = (mean^2 - mean * variance - mean^3) / variance;
   beta = (mean^3 - 2 * mean^2 + mean * variance + mean - variance) / variance;
+
 end
