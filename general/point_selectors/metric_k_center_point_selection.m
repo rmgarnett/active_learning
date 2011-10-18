8b1de2442eba56458444be4e479084bfbec172a1
rmgarnett <romanempire@gmail.com>
2011-10-18 08:20:33 -0400

    removing unnecessary inputs, will be handled via function handles

--- general/point_selectors/metric_k_center_point_selection.m
+++ general/point_selectors/metric_k_center_point_selection.m
@@ -1,15 +1,22 @@
 function test_ind = metric_k_center_point_selection(test, num_centers)
 
-  test_ind = 1;
-  candidate_ind = 2:size(test, 1);
+  test_ind = zeros(num_centers, 1);
+  test_ind(1) = 1;
 
   for i = 2:num_centers
-    min_distances = ...
-        min(sq_dist(test(test_ind, :)', test(candidate_ind, :)'));
+    current_ind = test_ind(1:(i - 1));
+    candidate_ind = setdiff(1:num_test, current_ind);
+    
+    if (numel(current_ind) > 1)
+      min_distances = ...
+          min(sq_dist(test(current_ind, :)', test(candidate_ind, :)'));
+    else
+      min_distances = ...
+          sq_dist(test(current_ind, :)', test(candidate_ind, :)');
+    end
     
     [~, ind] = max(min_distances);
-    test_ind = [test_ind ind];
-    candidate_ind(ind) = [];
+    test_ind(i) = ind;
   end
 
 end
