321ad27bd65688bbcbeea7b3597612044b8c0a5e
rmgarnett <romanempire@gmail.com>
2011-10-18 08:24:26 -0400

    optimal_utility was in truth only optimal in a certain case

--- general/utilities/optimal_utility.m
+++
@@ -1,9 +0,0 @@
-function utility = optimal_utility(data, responses, test, ...
-          proportion_estimation_function)
-  
-  [~, proportion_variance] = ...
-      proportion_estimation_function(data, responses, test);
-  
-  utility = -proportion_variance;
-
-end
