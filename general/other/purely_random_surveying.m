3b76dd5305fbe80d2971849171f9782ef44ec2fa
rmgarnett <romanempire@gmail.com>
2011-10-24 18:23:44 -0400

    minor refactoring

--- general/other/purely_random_surveying.m
+++ general/other/purely_random_surveying.m
@@ -1,4 +1,4 @@
-function [estimated_proportion proportion_variance in_train] = ...
+function [estimated_proportion selected] = ...
       purely_random_surveying(responses, in_train, num_evaluations)
 
   test_ind = find(~in_train);
@@ -7,6 +7,5 @@ function [estimated_proportion proportion_variance in_train] = ...
   in_train(test_ind(r(1:num_evaluations))) = true;
 
   estimated_proportion = mean(responses(in_train) == 1);
-  proportion_variance = 0;
 
 end
