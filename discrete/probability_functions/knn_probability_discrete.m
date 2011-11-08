c06a2201600a4de7b0766623f0310d11aa10dd25
rmgarnett <romanempire@gmail.com>
2011-11-07 23:04:28 -0500

    adding comments and in_train -> train_ind

--- discrete/probability_functions/knn_probability_discrete.m
+++ discrete/probability_functions/knn_probability_discrete.m
@@ -1,12 +1,35 @@
-function probabilities = knn_probability_discrete(responses, in_train, ...
+% function probabilities = knn_probability_discrete(responses, train_ind, ...
+%           test_ind, nearest_neighbors, weights, pseudocount)
+%
+% implementation of the probability function interface for a
+% k-nearest-neighbor classifier.
+%
+% inputs:
+%               data: an (n x d) matrix of input data
+%          responses: an (n x 1) vector of 0/1 responses
+%          train_ind: an index into data/responses indicating
+%                     the training data
+%           test_ind: an index into data/responses indicating
+%                     the test data
+%  nearest_neighbors: an (n x n) logical matrix indicating nearest
+%                     neighbors
+%  nearest_neighbors: an (n x n) matrix of weights
+%        pseudocount: a value in [0, 1] to use as a "pseudocount"
+%
+% outputs:
+%   probabilities: a vector of posterior probabilities for the test data
+%
+% copyright (c) roman garnett, 2011
+
+function probabilities = knn_probability_discrete(responses, train_ind, ...
           test_ind, nearest_neighbors, weights, pseudocount)
   
-  this_weights = nearest_neighbors(test_ind, in_train) .* ...
-                 weights(test_ind, in_train);
+  this_weights = nearest_neighbors(test_ind, train_ind) .* ...
+                           weights(test_ind, train_ind);
 
-  totals = sum(this_weights, 2) + 1;
+  total_weight = sum(this_weights, 2);
   
   probabilities = ...
-      (this_weights * responses(in_train) + pseudocount) ./ totals;
+      (this_weights * responses(train_ind) + pseudocount) ./ (total_weight + 1);
   
 end
