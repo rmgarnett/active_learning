429e39345f6273e9c698638675ac9ac97b1c5133
rmgarnett <romanempire@gmail.com>
2011-11-07 23:19:23 -0500

    moved

--- discrete/utility_functions/uncertainty_utility_discrete.m
+++
@@ -1,7 +0,0 @@
-function utility = uncertainty_utility_discrete(data, responses, ...
-          train_ind, probability_function)
-
-  probabilities = probability_function(data, responses, train_ind);
-  utility = min(abs(probabilities - (1 / 2)));
-
-end
