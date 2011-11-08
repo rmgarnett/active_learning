f31e353b48ce69f40c96ced3773a91ea19981b95
rmgarnett <romanempire@gmail.com>
2011-11-07 23:09:13 -0500

    adding comments

--- discrete/selection_functions/identity_point_selection.m
+++ discrete/selection_functions/identity_point_selection.m
@@ -1,3 +1,18 @@
+% function test_ind = identity_point_selection(train_ind)
+%
+% an implementation of the selection function interface to simply
+% select all points.
+%
+% inputs:
+%   train_ind: an index into data/responses indicating the 
+%              training points
+%
+% outputs:
+%    test_ind: an index into data/responses indicating the test
+%              points to use
+%
+% copyright (c) roman garnett, 2011
+
 function test_ind = identity_point_selection(in_train)
   
   test_ind = find(~in_train);
