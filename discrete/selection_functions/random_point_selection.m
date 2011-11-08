6baa00fbbb4ab6d9af668d78201deb09dffadd58
rmgarnett <romanempire@gmail.com>
2011-11-07 23:09:08 -0500

    initial import

---
+++ discrete/selection_functions/random_point_selection.m
@@ -0,0 +1,26 @@
+% function test_ind = random_point_selection(train_ind, num_test_points)
+%
+% an implementation of the selection function interface to select
+% a random subset of points
+%
+% inputs:
+%         train_ind: an index into data/responses indicating the 
+%                    training points
+%   num_test_points: the number of test points to select
+%
+% outputs:
+%    test_ind: an index into data/responses indicating the test
+%              points to use
+%
+% copyright (c) roman garnett, 2011
+
+function test_ind = random_point_selection(train_ind, num_test_points)
+  
+  test_ind = find(~train_ind);
+  num_test = nnz(test_ind);
+  r = randperm(num_test);
+  
+  % ensure we don't try to select more points than are available
+  test_ind = test_ind(r(1:min(num_test, num_test_points)));
+
+end
