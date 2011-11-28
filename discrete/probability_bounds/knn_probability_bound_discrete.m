8c50e76df0bc5b76887dd513ffd98f9551840ee0
Roman Garnett <romanempire@gmail.com>
2011-11-28 18:44:06 -0500

    formatting

---
+++ discrete/probability_bounds/knn_probability_bound_discrete.m
@@ -0,0 +1,34 @@
+% probability bound for a k-nearest-neighbor classifier.
+%
+% function probability_bound = knn_probability_bound_discrete(responses, train_ind, ...
+%           test_ind, weights, pseudocount)
+%
+% inputs:
+%          data: an (n x d) matrix of input data
+%     responses: an (n x 1) vector of 0/1 responses
+%     train_ind: an index into data/responses indicating
+%                the training points
+%      test_ind: an index into data/responses indicating
+%                the test points
+%       weights: an (n x n) matrix of weights
+%   pseudocount: a value in [0, 1] to use as a "pseudocount"
+%
+% outputs:
+%   probability_bound: an upper bound for the probabilities of the test data
+%
+% copyright (c) roman garnett, 2011
+
+function probability_bound = knn_probability_bound_discrete(responses, train_ind, ...
+          test_ind, weights, pseudocount)
+
+  this_weights = weights(test_ind, train_ind)
+  total_weight = sum(this_weights, 2)
+
+  max_weight = max(max(weights(test_ind, ~train_ind)))
+  
+  probability_bound = max( ...
+        (pseudocount + max_weight + this_weights * responses(train_ind)) ./ ...
+                  (1 + max_weight + total_weight) ...
+      );
+
+end
