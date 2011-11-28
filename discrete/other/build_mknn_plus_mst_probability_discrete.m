3dbcd7ab2a6200fb8d87b95db23b1009f751babd
Roman Garnett <romanempire@gmail.com>
2011-11-28 05:42:24 -0500

    cleaning up

--- discrete/other/build_mknn_plus_mst_probability_discrete.m
+++ discrete/other/build_mknn_plus_mst_probability_discrete.m
@@ -4,24 +4,22 @@ function probability_function = ...
 
   num_observations = size(data, 1);
 
-  [nearest_neighbors distances] = knnsearch(data, data, 'k', k + 1);
+  [nearest_neighbors, distances] = knnsearch(data, data, 'k', k + 1);
   nearest_neighbors = nearest_neighbors(:, 2:end)';
   distances = distances(:, 2:end)';
 
-  distances = sparse(kron((1:num_observations)', ones(k, 1)), ...
-                     nearest_neighbors(:), distances(:), num_observations, ...
-                     num_observations);
+  row_index = kron((1:num_observations)', ones(k, 1));
+  distances = sparse(row_index, nearest_neighbors(:), distances(:), ...
+                     num_observations, num_observations);
   weights = weight_function(distances);
-
-  nearest_neighbors = (weights > 0);
-
-  mst = logical(graphminspantree(nearest_neighbors));
+  
+  mst = logical(graphminspantree(weights > 0));
   mst = (mst | mst');
 
-  nearest_neighbors = ((nearest_neighbors & nearest_neighbors') | mst);
-  weights = weights .* nearest_neighbors;
+  weights((weights' == 0) & ~mst) = 0;
+  spy(weights)
 
   probability_function = @(data, responses, train_ind, test_ind) ...
       knn_probability_discrete(responses, train_ind, test_ind, ...
-                               nearest_neighbors, weights, pseudocount);
+                               weights, pseudocount);
 end
--- discrete/other/build_mknn_probability_discrete.m
+++ discrete/other/build_mknn_probability_discrete.m
@@ -3,23 +3,18 @@ function probability_function = build_mknn_probability_discrete(data, ...
 
   num_observations = size(data, 1);
 
-  [nearest_neighbors distances] = knnsearch(data, data, 'k', k + 1);
-
+  [nearest_neighbors, distances] = knnsearch(data, data, 'k', k + 1);
   nearest_neighbors = nearest_neighbors(:, 2:end)';
-  nearest_neighbors = logical(sparse(kron((1:num_observations)', ...
-          ones(k, 1)), nearest_neighbors(:), 1, num_observations, ...
-          num_observations));
-
   distances = distances(:, 2:end)';
-  distances = sparse(kron((1:num_observations)', ones(k, 1)), ...
-                     nearest_neighbors(:), distances(:), num_observations, ...
-                     num_observations);
+
+  row_index = kron((1:num_observations)', ones(k, 1));
+  distances = sparse(row_index, nearest_neighbors(:), distances(:), ...
+                     num_observations, num_observations);
   weights = weight_function(distances);
 
-  nearest_neighbors = (nearest_neighbors & nearest_neighbors');
-  weights = weights(nearest_neighbors);
+  weights(weights' == 0) = 0;
 
   probability_function = @(data, responses, train_ind, test_ind) ...
       knn_probability_discrete(responses, train_ind, test_ind, ...
-                               nearest_neighbors, weights, pseudocount);
+                               weights, pseudocount);
 end
