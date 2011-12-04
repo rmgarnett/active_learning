005f78d0b77a21920d448cd3ceff82bf3b113c82
rmgarnett <romanempire@gmail.com>
2011-12-04 11:44:41 -0500

    precomputing max(weights) for speed

--- discrete/bounds/knn_probability_bound_discrete.m
+++ discrete/bounds/knn_probability_bound_discrete.m
@@ -14,6 +14,7 @@
 %      test_ind: an index into data/responses indicating
 %                the test points
 %       weights: an (n x n) matrix of weights
+%   max_weights: precomputed max(weights)
 %   pseudocount: a value in [0, 1] to use as a "pseudocount"
 %
 % outputs:
@@ -22,12 +23,12 @@
 % copyright (c) roman garnett, 2011
 
 function bound = knn_probability_bound_discrete(responses, train_ind, ...
-          test_ind, weights, pseudocount)
+          test_ind, weights, max_weights, pseudocount)
 
   this_weights = weights(test_ind, train_ind);
   total_weight = sum(this_weights, 2);
 
-  max_weight = max(max(weights(test_ind, ~train_ind)));
+  max_weight = max(max_weights(~train_ind));
   
   bound = max( ...
         (pseudocount + max_weight + this_weights * responses(train_ind)) ./ ...
