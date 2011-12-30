9aa71c9b1ee37a785ec800bdf12fccdb9fb37ced
Roman Garnett <romanempire@gmail.com>
2011-12-30 08:09:29 -0500

    updating documentation

--- discrete/bounds/knn_probability_bound_discrete.m
+++ discrete/bounds/knn_probability_bound_discrete.m
@@ -1,23 +1,26 @@
 % function bound = knn_probability_bound_discrete(responses, train_ind, ...
 %           test_ind, weights, max_weights, pseudocount, num_positives)
+%
 % probability bound for a k-nearest-neighbor classifier. this
 % function provides a bound for
 %
 % \max_i p(y_i | x_i, D)
 %
-% after adding one additional point to the current training set,
+% after adding additional points to the current training set,
 % perhaps for use with probability_threshold_selection_function.
 %
 % inputs:
-%          data: an (n x d) matrix of input data
-%     responses: an (n x 1) vector of 0 / 1 responses
-%     train_ind: an index into data/responses indicating
-%                the training points
-%      test_ind: an index into data/responses indicating
-%                the test points
-%       weights: an (n x n) matrix of weights
-%   max_weights: precomputed max(weights)
-%   pseudocount: a value in [0, 1] to use as a "pseudocount"
+%            data: an (n x d) matrix of input data
+%       responses: an (n x 1) vector of 0 / 1 responses
+%       train_ind: an index into data/responses indicating
+%                  the training points
+%        test_ind: an index into data/responses indicating
+%                  the test points
+%         weights: an (n x n) matrix of weights
+%     max_weights: precomputed max(weights)
+%     pseudocount: a value in [0, 1] to use as a "pseudocount"
+%   num_positives: the number of additional positive responeses to
+%                  consider
 %
 % outputs:
 %   bound: an upper bound for the probabilities of the test data
