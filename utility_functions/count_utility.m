6416662ea8ef6273b68a2060b2a72dcb441c1ecf
Roman Garnett <romanempire@gmail.com>
2012-01-23 16:22:54 -0500

    updating for multi-class.

--- utility_functions/count_utility.m
+++ utility_functions/count_utility.m
@@ -1,21 +1,22 @@
 % simple "battleship" utility function
 %
-% u(D) = \sum_i y_i
+% u(D) = \sum_i \chi(y_i = 1)
 %
 % function utility = count_utility(responses, train_ind)
 %
 % inputs:
-%   responses: an (n x 1) vector of 0 / 1 responses
+%   responses: an (n x 1) vector of responses (class 1 is treated
+%              as "interesting")
 %   train_ind: a list of indices into responses
 %              indicating the training points
 %
 % outputs:
 %   utility: the utility of the selected points
 %
-% copyright (c) roman garnett, 2011
+% copyright (c) roman garnett, 2011--2012
 
 function utility = count_utility(responses, train_ind)
 
-  utility = nnz(responses(train_ind));
+  utility = nnz(responses(train_ind) == 1);
 
 end
