4122252f14a15f41d5c98a8fbe77ca815156e845
Roman Garnett <romanempire@gmail.com>
2012-01-23 16:22:07 -0500

    updating for multi-class.

--- selection_functions/identity_selection_function.m
+++ selection_functions/identity_selection_function.m
@@ -4,7 +4,7 @@
 %       identity_point_selection(responses, train_ind)
 %
 % inputs:
-%   responses: an (n x 1) vector of 0 / 1 responses
+%   responses: an (n x 1) vector of responses
 %   train_ind: a list of indices into data/responses
 %              indicating the labeled points
 %
@@ -12,12 +12,12 @@
 %    test_ind: a list of into data/responses indicating
 %              the points to test
 %
-% copyright (c) roman garnett, 2011
+% copyright (c) roman garnett, 2011--2012
 
 function test_ind = ...
       identity_selection_function(responses, train_ind)
-    
+
   test_ind = (1:numel(responses))';
   test_ind(train_ind) = [];
-  
+
 end
