995c9fa88ae85968d9f82b716c2cf50330157534
Roman Garnett <romanempire@gmail.com>
2012-01-23 16:24:47 -0500

    updating for multi-class.

--- utility_functions/proportion_variance_utility.m
+++ utility_functions/proportion_variance_utility.m
@@ -1,22 +1,24 @@
 % variance in the class proportion distribution used by active surveying
 %
-% u(D) = -var[ \sum_i y_i | D ]
+% u(D) = -var[ \sum_i \chi(y_i = 1) | D ]
 %
 % function utility = maximum_variance_utility(data, responses, ...
 %           train_ind, probability_function)
 %
 % inputs:
 %                             data: an (n x d) matrix of input data
-%                        responses: an (n x 1) vector of 0 / 1 responses
-%                        train_ind: a list of indices into into data/responses
-%                                   indicating the training points
+%                        responses: an (n x 1) vector of responses
+%                                   (class 1 is treated as "interesting")
+%                        train_ind: a list of indices into into
+%                                   data/responses indicating the
+%                                   training points
 %   proportion_estimation_function: a handle to a proportion
 %                                   estimation function
 %
 % outputs:
 %   utility: the utility of the selected points
 %
-% copyright (c) roman garnett, 2011
+% copyright (c) roman garnett, 2011--2012
 
 function utility = proportion_variance_utility(data, responses, ...
           train_ind, proportion_estimation_function)
