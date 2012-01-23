37df65ad624da83dcd3336e70993f4f037fa3ac1
Roman Garnett <romanempire@gmail.com>
2012-01-23 16:20:47 -0500

    updating for multi-class.

--- selection_functions/random_selection_function.m
+++ selection_functions/random_selection_function.m
@@ -4,7 +4,7 @@
 %       random_selection_function(train_ind, num_test_points)
 %
 % inputs:
-%         responses: an (n x 1) vector of 0 / 1 responses
+%         responses: an (n x 1) vector of responses
 %         train_ind: a list of indices into data/responses
 %                    indicating the training points
 %   num_test_points: the number of test points to select
@@ -13,15 +13,15 @@
 %    test_ind: a list of indices into data/responses
 %              indicating the points to test
 %
-% copyright (c) roman garnett, 2011
+% copyright (c) roman garnett, 2011--2012
 
 function test_ind = ...
       random_selection_function(responses, train_ind, num_test_points)
-  
+
   test_ind = identity_selection_function(responses, train_ind);
   num_test = numel(test_ind);
   r = randperm(num_test);
-  
+
   % ensure we don't try to select more points than are available
   test_ind = r(1:min(num_test, num_test_points));
 
