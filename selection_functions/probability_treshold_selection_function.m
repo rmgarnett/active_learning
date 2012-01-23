6d113830cd913401fa07b1fd35e7585a15f16565
Roman Garnett <romanempire@gmail.com>
2012-01-23 16:18:19 -0500

    updating to handle multi-class

--- selection_functions/probability_treshold_selection_function.m
+++ selection_functions/probability_treshold_selection_function.m
@@ -3,18 +3,18 @@
 %
 % where
 %                   data: an (n x d) matrix of input data
-%              responses: an (n x 1) vector of 0 / 1 responses
+%              responses: an (n x 1) vector of responses
 %              train_ind: a list of indices into data/responses
 %                         indicating the training points
-%   probability_function: a function handle providing a probability function 
+%   probability_function: a function handle providing a probability function
 %              threshold: a value in [0, 1]; points with
 %                         probability greater than or equal to this
-%                         are selected
+%                         (for any class) are selected
 %
 %   test_ind: a list of indices into data/responses indicating
 %             the points to test
 %
-% copyright (c) roman garnett, 2011
+% copyright (c) roman garnett, 2011--2012
 
 function test_ind = probability_treshold_selection_function(data, ...
           responses, train_ind, probability_function, threshold)
@@ -23,6 +23,6 @@ function test_ind = probability_treshold_selection_function(data, ...
   probabilities = ...
       probability_function(data, responses, train_ind, test_ind);
 
-  test_ind = find(probabilities >= threshold);
-  
+  test_ind = any(probabilities >= threshold, 2);
+
 end
