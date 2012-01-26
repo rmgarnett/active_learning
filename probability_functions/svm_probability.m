e600617e38f7522aa58550d0110c02163d65623d
rmgarnett <romanempire@gmail.com>
2012-01-25 21:19:34 -0500

    documentation

--- probability_functions/svm_probability.m
+++ probability_functions/svm_probability.m
@@ -7,10 +7,10 @@
 %        data: an (n x d) matrix of input data
 %   responses: an (n x 1) vector of responses (class 1 is tested
 %              against "any other class")
-%   train_ind: a list of indices into data/responses
-%              indicating the training points
-%    test_ind: a list of indices into data/responses
-%              indicating the test points
+%   train_ind: a list of indices into data/responses indicating
+%              the training points
+%    test_ind: a list of indices into data/responses indicating
+%              the test points
 %    varargin: all remaining inputs are passed to svmtrain
 %
 % outputs:
