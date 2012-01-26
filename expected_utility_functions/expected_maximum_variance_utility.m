ca18f9c3ec04b37dfcd70344412a16048d2acf46
rmgarnett <romanempire@gmail.com>
2012-01-25 21:17:12 -0500

    formatting

--- expected_utility_functions/expected_maximum_variance_utility.m
+++ expected_utility_functions/expected_maximum_variance_utility.m
@@ -25,8 +25,7 @@
 function expected_utilities = expected_maximum_variance_utility(data, ...
           responses, train_ind, test_ind, probability_function)
 
-  probabilities = ...
-      probability_function(data, responses, train_ind, test_ind);
+  probabilities = probability_function(data, responses, train_ind, test_ind);
   expected_utilities = -max(1 - max(probabilities, [], 2));
 
 end
