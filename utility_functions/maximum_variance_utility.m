d661725ac73001b48128647f55be2574da347636
rmgarnett <romanempire@gmail.com>
2012-01-25 21:33:20 -0500

    documentation and updating for multi-class

--- utility_functions/maximum_variance_utility.m
+++ utility_functions/maximum_variance_utility.m
@@ -1,6 +1,6 @@
-% maximum variance loss function used by uncertainty sampling
+% maximum variance loss function used by uncertainty sampling.
 %
-% u(D) = -\max_i var p(y_i = 1 | x_i, D)
+% u(D) = -\max_i (1 - p(argmax_y y_i = y | x_i, D))
 %
 % function utility = maximum_variance_utility(data, responses, train_ind, ...
 %           probability_function)
@@ -8,8 +8,8 @@
 % inputs:
 %        data: an (n x d) matrix of input data
 %   responses: an (n x 1) vector of responses
-%   train_ind: a list of indices into data/responses
-%              indicating the training points
+%   train_ind: a list of indices into data/responses indicating the
+%              training points
 %
 % outputs:
 %   utility: the utility of the selected points
@@ -21,6 +21,6 @@ function utility = maximum_variance_utility(data, responses, train_ind, ...
 
   test_ind = identity_selection_function(responses, train_ind);
   probabilities = probability_function(data, responses, train_ind, test_ind);
-  utility = min(max(abs(probabilities - (1 / 2))));
+  utility = -max(1 - max(probabilities, [], 2));
 
 end
