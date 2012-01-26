d045c7714749121e5ddd828ee535df9d06785579
rmgarnett <romanempire@gmail.com>
2012-01-25 20:57:07 -0500

    documentation

--- probability_functions/knn_probability.m
+++ probability_functions/knn_probability.m
@@ -7,10 +7,10 @@
 %          data: an (n x d) matrix of input data
 %     responses: an (n x 1) vector of responses (class 1 is tested
 %                against "any other class")
-%     train_ind: a list of indices into data/responses
-%                indicating the training points
-%      test_ind: a list of indices into data/responses
-%                indicating the test points
+%     train_ind: a list of indices into data/responses indicating
+%                the training points
+%      test_ind: a list of indices into data/responses indicating
+%                the test points
 %       weights: an (n x n) matrix of weights
 %   pseudocount: a value in [0, 1] to use as a "pseudocount"
 %
