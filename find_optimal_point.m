b1d598bc0be9a6a9c1d41ad84c5f597402aafd8f
Roman Garnett <romanempire@gmail.com>
2012-01-25 20:46:26 -0500

    changing for multi-class

--- find_optimal_point.m
+++ find_optimal_point.m
@@ -1,3 +1,10 @@
+<<<<<<< Updated upstream
+=======
+% function [best_utility, best_ind] = find_optimal_point(data, responses, ...
+%           train_ind, selection_functions, probability_function, ...
+%           expected_utility_function, lookahead)
+%
+>>>>>>> Stashed changes
 % finds the optimal next point to add to a dataset for active learning
 % on a set of discrete points for a particular utility function and
 % lookahead.  this function supports using user-defined:
@@ -18,7 +25,7 @@
 %
 % inputs:
 %                        data: an (n x d) matrix of input data
-%                   responses: an (n x 1) vector of 0 / 1 responses
+%                   responses: an (n x 1) vector of responses
 %                   train_ind: a list of indices into data/responses
 %                              indicating the labeled points
 %         selection_functions: a cell array of selection functions
@@ -33,9 +40,9 @@
 %   best_utility: the expected utility of the best point found
 %       best_ind: the index of the best point found
 %
-% copyright (c) roman garnett, 2011
+% copyright (c) roman garnett, 2011--2012
 
-function [best_utility best_ind] = find_optimal_point(data, responses, ...
+function [best_utility, best_ind] = find_optimal_point(data, responses, ...
           train_ind, selection_functions, probability_function, ...
           expected_utility_function, lookahead)
 
@@ -67,28 +74,31 @@ function [best_utility best_ind] = find_optimal_point(data, responses, ...
   probabilities = probability_function(data, responses, train_ind, test_ind);
   expected_utilities = zeros(num_test, 1);
 
+<<<<<<< Updated upstream
   parfor j = 1:num_test
     fake_train_ind = [train_ind; test_ind(j)];
 
+=======
+  for i = 1:num_test
+    fake_train_ind = [train_ind; test_ind(i)];
+>>>>>>> Stashed changes
     fake_responses = responses;
 
-    % add a fake "true" observation for this test point and
-    % calculate the expected utility, calling this function
-    % recursively with new point and (lookahead - 1)
-    fake_responses(test_ind(j)) = true;
-    utility_true = find_optimal_point(data, fake_responses, ...
-            fake_train_ind, selection_functions, probability_function, ...
-            expected_utility_function, lookahead - 1);
+    num_classes = max(responses);
+    fake_utilities = zeros(num_classes, 1);
 
-    % add a fake "false" observation for this test point
-    fake_responses(test_ind(j)) = false;
-    utility_false = find_optimal_point(data, fake_responses, ...
-            fake_train_ind, selection_functions, probability_function, ...
-            expected_utility_function, lookahead - 1);
+    for fake_response = 1:num_classes
+      % add a fake observation for this test point and calculate the
+      % expected utility, calling this function recursively with new
+      % point and (lookahead - 1)
+      fake_responses(test_ind(i)) = fake_response;
 
-    % calculate the overall expected utility
-    expected_utilities(j) = probabilities(j)  * utility_true + ...
-                       (1 - probabilities(j)) * utility_false;
+      fake_utilities(fake_response) = find_optimal_point(data, ...
+              fake_responses, fake_train_ind, selection_functions, ...
+              probability_function, expected_utility_function, lookahead - 1);
+    end
+
+    expected_utilities(i) = probabilities(i, :) * fake_utilities;
   end
 
   [best_utility best_ind] = max(expected_utilities);
