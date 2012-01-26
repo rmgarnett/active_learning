0072816c82516faf48b62b4e4656b6e6f7a68150
rmgarnett <romanempire@gmail.com>
2012-01-25 21:27:17 -0500

    documentation

--- selection_functions/probability_treshold_selection_function.m
+++ selection_functions/probability_treshold_selection_function.m
@@ -1,20 +1,23 @@
+% selects points with posterior probability above a specified threshold.
+%
 % function test_ind = probability_treshold_selection_function(data, ...
 %           responses, train_ind, probability_function, threshold)
 %
 % where
 %                   data: an (n x d) matrix of input data
-%              responses: an (n x 1) vector of 0 / 1 responses
-%              train_ind: a list of indices into data/responses
-%                         indicating the training points
-%   probability_function: a function handle providing a probability function 
-%              threshold: a value in [0, 1]; points with
+%              responses: an (n x 1) vector of responses
+%              train_ind: a list of indices into data/responses indicating
+%                         the training points
+%   probability_function: a function handle providing a probability
+%                         function 
+%              threshold: a value in [0, 1]; points with posterior
 %                         probability greater than or equal to this
 %                         are selected
 %
 %   test_ind: a list of indices into data/responses indicating
 %             the points to test
 %
-% copyright (c) roman garnett, 2011
+% copyright (c) roman garnett, 2011--2012
 
 function test_ind = probability_treshold_selection_function(data, ...
           responses, train_ind, probability_function, threshold)
