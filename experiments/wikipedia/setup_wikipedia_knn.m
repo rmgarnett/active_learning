67c740a53a0713238dcebbcc5722249cb79fa7f9
Roman Garnett <romanempire@gmail.com>
2012-02-04 22:47:07 -0500

    simplifying experiments

--- experiments/wikipedia/setup_wikipedia_knn.m
+++ experiments/wikipedia/setup_wikipedia_knn.m
@@ -1,5 +1,3 @@
-pseudocount = 0.1;
-
 neighbors = neighbors';
 similarities = similarities';
 
@@ -12,3 +10,7 @@ max_weights = full(max_weights);
 
 probability_function = @(data, responses, train_ind, test_ind) ...
     knn_probability(responses, train_ind, test_ind, weights, pseudocount);
+
+probability_bound = @(data, responses, train_ind, test_ind, num_positives) ...
+    knn_probability_bound(responses, train_ind, test_ind, weights, ...
+                          max_weights, pseudocount, num_positives);
