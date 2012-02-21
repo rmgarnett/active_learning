8dbf712b8b5adf0ee3545eadac0218d2dd3ce04a
Roman Garnett <romanempire@gmail.com>
2012-02-21 23:13:17 +0100

    proportion_estimation_function -> proportion_estimator

--- utility_functions/proportion_variance_utility.m
+++ utility_functions/proportion_variance_utility.m
@@ -2,18 +2,17 @@
 %
 % u(D) = -var[ \sum_i \chi(y_i = 1) | D ]
 %
-% function utility = maximum_variance_utility(data, responses, ...
-%           train_ind, probability_function)
+% function utility = proportion_variance_utility(data, responses, ...
+%           train_ind, proportion_estimator)
+%
 %
 % inputs:
-%                             data: an (n x d) matrix of input data
-%                        responses: an (n x 1) vector of responses
-%                                   (class 1 is treated as "interesting")
-%                        train_ind: a list of indices into into
-%                                   data/responses indicating the
-%                                   training points
-%   proportion_estimation_function: a handle to a proportion
-%                                   estimation function
+%                   data: an (n x d) matrix of input data
+%              responses: an (n x 1) vector of responses (class 1 is
+%                         treated as "interesting")
+%              train_ind: a list of indices into into data/responses
+%                         indicating the training points
+%   proportion_estimator: a handle to a proportion estimation function
 %
 % outputs:
 %   utility: the utility of the selected points
@@ -21,10 +20,9 @@
 % copyright (c) roman garnett, 2011--2012
 
 function utility = proportion_variance_utility(data, responses, ...
-          train_ind, proportion_estimation_function)
+          train_ind, proportion_estimator)
 
-  [~, proportion_variance] = proportion_estimation_function(data, ...
-          responses, train_ind);
+  [~, proportion_variance] = proportion_estimator(data, responses, train_ind);
   utility = -proportion_variance;
 
 end
