5f339b4f9da8a4157614cbf6131e2b28ab60687e
Roman Garnett <romanempire@gmail.com>
2012-02-28 10:47:52 +0100

    slight formatting

--- utility_functions/utility_functions.m
+++ utility_functions/utility_functions.m
@@ -1,13 +1,13 @@
-% utility functions measure the utility of a given dataest.
-% utility functions have the interface:
+% utility functions measure the utility of a given dataest.  utility
+% functions have the interface:
 %
 % utility = utility_function(data, responses, train_ind)
 %
 % inputs:
 %        data: an (n x d) matrix of input data
 %   responses: an (n x 1) vector of responses
-%   train_ind: a list of indices into data/responses
-%              indicating the training points
+%   train_ind: a list of indices into data/responses indicating the
+%              training points
 %
 % outputs:
 %   utility: the utility of the selected points
