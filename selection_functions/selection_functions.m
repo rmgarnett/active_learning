639fec7e2c1b9f7c75a5ccccd99e7450e56c2e09
Roman Garnett <romanempire@gmail.com>
2012-02-28 10:59:43 +0100

    minor formatting

--- selection_functions/selection_functions.m
+++ selection_functions/selection_functions.m
@@ -7,11 +7,11 @@
 % inputs:
 %        data: an (n x d) matrix of input data
 %   responses: an (n x 1) vector of responses
-%   train_ind: a list of indices into data/responses
-%              indicating the training points
+%   train_ind: a list of indices into data/responses indicating the
+%              training points
 %
 % outputs:
-%    test_ind: an list of indices into data/responses
-%              indicating the points to test
+%    test_ind: an list of indices into data/responses indicating the
+%              points to test
 %
 % copyright (c) roman garnett, 2011--2012
