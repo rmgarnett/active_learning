cadff648f4e637a308259ab7f502274990777cad
Roman Garnett <romanempire@gmail.com>
2012-03-01 12:57:58 +0100

    bug: typo, reponses -> responses

--- probability_functions/knn_probability.m
+++ probability_functions/knn_probability.m
@@ -25,7 +25,7 @@ function probabilities = knn_probability(responses, train_ind, ...
           test_ind, weights, pseudocount)
 
   % transform responses to handle multi-class
-  reponses(responses ~= 1) = 0;
+  responses(responses ~= 1) = 0;
 
   this_weights = weights(test_ind, train_ind);
   total_weight = sum(this_weights, 2);
