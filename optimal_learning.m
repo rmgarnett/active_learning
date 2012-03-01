5a340ea183b3e125a49775be7689968035d3d254
Roman Garnett <romanempire@gmail.com>
2012-03-01 15:51:14 +0100

    documentation, formatting, removing problem structure

--- optimal_learning.m
+++ optimal_learning.m
@@ -2,37 +2,33 @@
 % particular utility function and lookahead.  this function supports
 % using user-defined:
 %
+% - utility functions, which calculate the utility of a selected set
+%   of points
+% - probability functions, which assign probabilities to indicated
+%   test data from the current training set
 % - selection functions, which specify which among the unlabeled
 %   points should have their expected utilities evaluated. this
 %   implementation allows multiple selection functions to be used,
 %   should different ones be desired for different lookaheads.
-% - probability functions, which assign probabilities to indicated
-%   test data from the current training set
-% - utility functions, which calculate the utility of a selected set
-%   of points
 %
 % function [chosen_ind, utilities] = optimal_learning(data, responses, ...
 %           train_ind, problem, num_evaluations, lookahead, verbose)
 %
 % inputs:
-%              data: an (n x d) matrix of input data
-%         responses: an (n x 1) vector of responses
-%         train_ind: a list of indices into data/responses indicating the
-%                    starting labeled points
-%           problem: a structure defining the active learning problem,
-%                    with fields:
-%
-%           selection_functions: a cell array of selection functions
-%                                to use. if lookahead = k, then the
-%                                min(k, numel(selection_functions))th
-%                                element of this array will be used.
-%          probability_function: the probability function to use
-%              utility_function: the utility function to use
-%
-%   num_evaluations: the number of points to select
-%         lookahead: the number of steps to look ahead
-%           verbose: true to print status after each evaluation
-%                    (default: false)
+%                   data: an (n x d) matrix of input data
+%              responses: an (n x 1) vector of responses
+%              train_ind: a list of indices into data/responses
+%                         indicating the starting labeled points
+%    selection_functions: a cell array of selection functions to
+%                         use. if lookahead = k, then the
+%                         min(k, numel(selection_functions))th
+%                         element of this array will be used.
+%   probability_function: the probability function to use
+%       utility_function: the utility function to use
+%              lookahead: the number of steps to look ahead at each step
+%        num_evaluations: the number of points to select
+%                verbose: a boolean, true to print status after
+%                         each evaluation (default: false)
 %
 % outputs:
 %   chosen_ind: a list of indices of the chosen datapoints, in order
@@ -42,7 +38,8 @@
 % copyright (c) roman garnett, 2011--2012
 
 function [chosen_ind, utilities] = optimal_learning(data, responses, ...
-          train_ind, problem, num_evaluations, lookahead, verbose)
+          train_ind, utility_function, probability_function, ...
+          selection_functions, lookahead, num_evaluations, verbose)
 
   % set verbose to false if not defined
   verbose = exist('verbose', 'var') && verbose;
@@ -52,7 +49,7 @@ function [chosen_ind, utilities] = optimal_learning(data, responses, ...
 
   for i = 1:num_evaluations
     if (verbose)
-      start = tic;
+      tic();
       fprintf('point %i: ', i);
     end
 
@@ -62,18 +59,18 @@ function [chosen_ind, utilities] = optimal_learning(data, responses, ...
     % find the optimal next point to add given the current training set
     % and chosen utility function
     [~, best_ind] = find_optimal_point(data, responses, ...
-            train_ind, problem, lookahead);
+            train_ind, utility_function, probability_function, ...
+            selection_functions, lookahead)
 
     % add the selected point and measure our current success
     chosen_ind(i) = best_ind;
     train_ind = [train_ind; best_ind];
-
-    utilities(i) = problem.utility_function(data, responses, train_ind);
+    utilities(i) = utility_function(data, responses, train_ind);
 
     if (verbose)
-      elapsed = toc(start);
+      elapsed = toc();
       fprintf(['lookahead: %i, ' ...
-               'true utility: %.2f, ' ...
+               'utility: %.2f, ' ...
                'took: %.2fs.\n'], ...
               lookahead, ...
               utilities(i), ...
