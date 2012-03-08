bfeb5e74bfe966469359944151bd1797baf33b45
Roman Garnett <romanempire@gmail.com>
2012-03-08 15:08:42 +0100

    documentation and refactoring to be slightly clearer

--- implementations/uncertainty_sampling/minimum_negative_entropy_utility.m
+++ implementations/uncertainty_sampling/minimum_negative_entropy_utility.m
@@ -2,8 +2,8 @@
 %
 %   u(D) = \min_i \sum_j p(y_i = j | x_i, D) log p(y_i = j | x_i, D)
 %
-% function utility = minimum_negentropy_utility(data, responses, train_ind, ...
-%           probability_function)
+% function utility = minimum_negative_entropy_utility(data, responses, ...
+%          train_ind, probability_function)
 %
 % inputs:
 %                   data: an (n x d) matrix of input data
@@ -17,14 +17,14 @@
 %
 % copyright (c) roman garnett, 2011--2012
 
-function utility = minimum_negentropy_utility(data, responses, train_ind, ...
-          probability_function)
+function utility = minimum_negative_entropy_utility(data, responses, ...
+          train_ind, probability_function)
 
   test_ind = identity_selector(responses, train_ind);
 
   probabilities = probability_function(data, responses, train_ind, test_ind);
-  negative_entropies = sum(probabilities .* log(probabilities), 2);
+  entropies = sum(probabilities .* log(probabilities), 2);
 
-  utility = min(negative_entropies);
+  utility = -max(entropies);
 
 end
