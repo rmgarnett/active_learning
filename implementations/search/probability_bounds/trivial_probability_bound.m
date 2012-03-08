fac5fbb92a1a7974a23a3534932c1d41c857fb7f
Roman Garnett <romanempire@gmail.com>
2012-03-08 15:50:41 +0100

    adding search implementation

---
+++ implementations/search/probability_bounds/trivial_probability_bound.m
@@ -0,0 +1,16 @@
+% trivial probability bound
+%
+%   bound = 1
+%
+% function bound = trivial_utility_bound
+%
+% outputs:
+%   bound: an upper bound for the probabilities of the test data
+%
+% copyright (c) roman garnett, 2011--2012
+
+function bound = trivial_probability_bound
+
+  bound = 1;
+
+end
