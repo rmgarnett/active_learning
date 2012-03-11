70f9edbc1e2e82fc9c907331b6087951849643e1
Roman Garnett <romanempire@gmail.com>
2012-03-11 13:03:48 +0000

    random sampling implementation

---
+++ implementations/random_sampling/random_sampling.m
@@ -0,0 +1,15 @@
+function [chosen_ind, utilities] = random_sampling(data, responses, ...
+          train_ind, utility_function, num_evaluations, verbose)
+
+  % set verbose to false if not defined
+  verbose = exist('verbose', 'var') && verbose;
+
+  probability_function = [];
+  selection_functions = [];
+  lookahead = 0;
+
+  [chosen_ind, utilities] = optimal_learning(data, responses, ...
+          train_ind, utility_function, probability_function, ...
+          selection_functions, lookahead, num_evaluations, verbose);
+
+end
