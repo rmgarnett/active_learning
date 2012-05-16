d29cd200095c0dbea4d5abbaf6f93494b809ac24
rmgarnett <romanempire@gmail.com>
2012-05-16 13:29:32 -0400

    allow selection functions

--- implementations/random_sampling/random_sampling.m
+++ implementations/random_sampling/random_sampling.m
@@ -1,11 +1,11 @@
 function [chosen_ind, utilities] = random_sampling(data, responses, ...
-          train_ind, utility_function, num_evaluations, verbose)
+          train_ind, utility_function, selection_functions, ...
+          num_evaluations, verbose)
 
   % set verbose to false if not defined
   verbose = exist('verbose', 'var') && verbose;
 
   probability_function = [];
-  selection_functions = [];
   lookahead = 0;
 
   [chosen_ind, utilities] = optimal_learning(data, responses, ...
