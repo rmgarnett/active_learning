f362f871c3822294787c4f9e60d57e7a0b7f8317
rmgarnett <romanempire@gmail.com>
2012-05-16 13:46:10 -0400

    fixing to have test_ind

--- implementations/uncertainty_sampling/calculate_entropies.m
+++ implementations/uncertainty_sampling/calculate_entropies.m
@@ -1,9 +1,11 @@
 function entropies = calculate_entropies(data, responses, train_ind, ...
-          probability_function)
+          test_ind, probability_function)
 
-  test_ind = identity_selector(responses, train_ind);
-
+  unlabeled_ind = identity_selector(responses, train_ind);
+  entropies = zeros(size(data, 1), 1);
+  
   probabilities = probability_function(data, responses, train_ind, test_ind);
-  entropies = sum(probabilities .* log(probabilities), 2);
-
+  entropies(test_ind) = -sum(probabilities .* log(probabilities), 2);
+  entropies = entropies(unlabeled_ind);
+  
 end
