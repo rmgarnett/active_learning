35daa4a3167baacfa386a0421654a5c054c52916
rmgarnett <romanempire@gmail.com>
2012-05-16 13:28:44 -0400

    bug! triu -> triu(., 1)

--- implementations/graph_tools/extract_upper_triangle_vector.m
+++ implementations/graph_tools/extract_upper_triangle_vector.m
@@ -1,6 +1,6 @@
 function upper_triangle_vector = extract_upper_triangle_vector(matrix)
 
-  upper_triangle = triu(matrix);
-  upper_triangle_vector = upper_triangle(triu(true(size(matrix))));
+  upper_triangle = triu(matrix, 1);
+  upper_triangle_vector = upper_triangle(triu(true(size(matrix)), 1));
 
 end
