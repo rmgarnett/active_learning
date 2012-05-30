b5501cc3b05b745cb31c60a271dc81adae047c0b
rmgarnett <romanempire@gmail.com>
2012-05-30 16:44:16 -0400

    refactoring and simplification

--- probability_functions/label_propagation_probability.m
+++ probability_functions/label_propagation_probability.m
@@ -1,33 +1,26 @@
 % copyright (c) roman garnett, 2011--2012
 
-function probabilities = label_propagation_probability(data, responses, ...
-          train_ind, test_ind, tolerance, max_iterations)
+function probabilities = label_propagation_probability(A, node_labels, ...
+          train_ind, test_ind, num_iterations)
 
-  num_points  = size(data, 1);
+  num_points  = size(A, 1);
   num_train   = numel(train_ind);
-  num_classes = max(responses);
+  num_classes = max(node_labels);
 
   % preallocate the rows in the label probability matrix for the
   % labeled nodes
   train_rows = zeros(num_train, num_classes);
   for i = 1:numel(train_ind)
     ind = train_ind(i);
-    train_rows(i, responses(ind)) = 1;
+    train_rows(i, node_labels(ind)) = 1;
   end
 
-  probabilities = (1 / num_classes) * ones(num_points, num_classes);
-  error = Inf;
-  num_iterations = 0;
-  while (error > tolerance && num_iterations < max_iterations)
+  probabilities = repmat(mean(train_rows), [num_points, 1]);
+
+  for i = 1:num_iterations;
     % "pull-back" known labels
     probabilities(train_ind, :) = train_rows;
-
-    new_probabilities = data * probabilities;
-
-    error = norm(new_probabilities(test_ind, :) - probabilities(test_ind, :));
-    probabilities = new_probabilities;
-
-    num_iterations = num_iterations + 1;
+    probabilities = A * probabilities;
   end
 
   probabilities = probabilities(test_ind, :);
