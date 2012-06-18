0db66adfcb8c5b13cf49d429494696cdf1dffe38
rmgarnett <romanempire@gmail.com>
2012-06-18 03:58:22 -0400

    responses -> labels

--- selection_functions/random_selector.m
+++ selection_functions/random_selector.m
@@ -1,22 +1,22 @@
 % selects a random subset of points.
 %
-% function test_ind = random_selector(responses, train_ind, num_test_points)
+% function test_ind = random_selector(labels, train_ind, num_test_points)
 %
 % inputs:
-%         responses: an (n x 1) vector of responses
-%         train_ind: a list of indices into data/responses indicating
-%                    the training points
+%         labels: an (n x 1) vector of labels
+%         train_ind: a list of indices into data/labels indicating the
+%                    training points
 %   num_test_points: the number of test points to select
 %
 % outputs:
-%    test_ind: a list of indices into data/responses
-%              indicating the points to test
+%    test_ind: a list of indices into data/labels indicating the
+%              points to test
 %
 % copyright (c) roman garnett, 2011--2012
 
-function test_ind = random_selector(responses, train_ind, num_test_points)
+function test_ind = random_selector(labels, train_ind, num_test_points)
 
-  test_ind = identity_selector(responses, train_ind);
+  test_ind = identity_selector(labels, train_ind);
   num_test = numel(test_ind);
   r = randperm(num_test);
 
