a3497c6997d5a8514d5df3ff01ea9834643b8250
rmgarnett <romanempire@gmail.com>
2012-06-18 03:52:06 -0400

    responses -> labels

--- selection_functions/selection_functions.m
+++ selection_functions/selection_functions.m
@@ -2,16 +2,16 @@
 % indicates which of the unlabeld points should be considered for
 % addition at this time.  selection functions have the interface:
 %
-% test_ind = selection_function(data, responses, train_ind)
+% test_ind = selection_function(data, labels, train_ind)
 %
 % inputs:
 %        data: an (n x d) matrix of input data
-%   responses: an (n x 1) vector of responses
-%   train_ind: a list of indices into data/responses indicating the
+%      labels: an (n x 1) vector of labels
+%   train_ind: a list of indices into data/labels indicating the
 %              training points
 %
 % outputs:
-%    test_ind: an list of indices into data/responses indicating the
+%    test_ind: an list of indices into data/labels indicating the
 %              points to test
 %
 % copyright (c) roman garnett, 2011--2012
