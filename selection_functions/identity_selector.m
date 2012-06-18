998f119ac192e908982da3c88f0a225cf847e36b
rmgarnett <romanempire@gmail.com>
2012-06-18 03:52:57 -0400

    responses -> labels

--- selection_functions/identity_selector.m
+++ selection_functions/identity_selector.m
@@ -1,21 +1,21 @@
 % selects all points.
 %
-% function test_ind = identity_selector(responses, train_ind)
+% function test_ind = identity_selector(labels, train_ind)
 %
 % inputs:
-%   responses: an (n x 1) vector of responses
-%   train_ind: a list of indices into data/responses indicating the
+%      labels: an (n x 1) vector of labels
+%   train_ind: a list of indices into data/labels indicating the
 %              labeled points
 %
 % outputs:
-%    test_ind: a list of indices into data/responses indicating the
+%    test_ind: a list of indices into data/labels indicating the
 %               points to test
 %
 % copyright (c) roman garnett, 2011--2012
 
-function test_ind = identity_selector(responses, train_ind)
+function test_ind = identity_selector(labels, train_ind)
 
-  test_ind = (1:numel(responses))';
+  test_ind = (1:numel(labels))';
   test_ind(train_ind) = [];
 
 end
