52c73368a14ec6288fcc5e2c5d8ae8a500bc5b86
rmgarnett <romanempire@gmail.com>
2012-06-18 04:34:54 -0400

    responses -> labels, objective_function -> score_function

--- selection_functions/maximum_value_selector.m
+++ selection_functions/maximum_value_selector.m
@@ -1,37 +1,36 @@
 % selects the single point that maximizes a given function of the test
 % points.
 %
-% function test_ind = maximum_value_selector(data, responses, train_ind, ...
+% function test_ind = maximum_value_selector(data, labels, train_ind, ...
 %           objective_function)
 %
 % where
-%                   data: an (n x d) matrix of input data
-%              responses: an (n x 1) vector of responses
-%              train_ind: a list of indices into data/responses indicating
-%                         the training points
-%    objectiove_function: a function handle providing a function
-%                         with the interface
+%             data: an (n x d) matrix of input data
+%           labels: an (n x 1) vector of labels
+%        train_ind: a list of indices into data/labels indicating the
+%                   training points
+%   score_function: a function handle providing a function with the
+%                   interface
 %
-%                         values = objective_function(data, responses, ...
-%                                                     train_ind)
+%                   scores = score_function(data, labels, train_ind)
 %
-%                         this function is expected to retrun a value
-%                         for every data point not in train_ind, and
-%                         ultimately the currently unlabeled point
-%                         that maximizes this function will be chosen.
+%                   this function is expected to retrun a value for
+%                   every data point not in train_ind, and ultimately
+%                   the currently unlabeled point that maximizes this
+%                   function will be chosen.
 %
-%   test_ind: an index into data/responses indicating the point
-%             with the maximal value of the objective function
+%   test_ind: an index into data/labels indicating the point
+%             with the maximal value of the score function
 %
 % copyright (c) roman garnett, 2012
 
-function test_ind = maximum_value_selector(data, responses, train_ind, ...
-          objective_function)
+function test_ind = maximum_score_selector(data, labels, train_ind, ...
+          score_function)
 
-  test_ind = identity_selector(responses, train_ind);
+  test_ind = identity_selector(labels, train_ind);
 
-  values = objective_function(data, responses, train_ind);
-  [~, best_ind] = max(values);
+  scores = score_function(data, labels, train_ind);
+  [~, best_ind] = max(scores);
 
   test_ind = test_ind(best_ind);
 
