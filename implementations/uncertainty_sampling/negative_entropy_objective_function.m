54f3221cf44f86b4336df6b815b3f89ad0724ec5
rmgarnett <romanempire@gmail.com>
2012-07-11 15:40:51 -0400

    no longer needed

--- implementations/uncertainty_sampling/negative_entropy_objective_function.m
+++
@@ -1,9 +0,0 @@
-function values = negative_entropy_objective_function(data, responses, ...
-          train_ind, probability_function)
-
-  test_ind = identity_selector(responses, train_ind);
-
-  probabilities = probability_function(data, responses, train_ind, test_ind);
-  values = -sum(probabilities .* log(probabilities), 2);
-
-end
