6543edd841485d05600ad91f66b98a87816233f1
rmgarnett <romanempire@gmail.com>
2012-07-11 15:42:57 -0400

    documentation

--- utility_functions/sum_utility.m
+++ utility_functions/sum_utility.m
@@ -3,7 +3,7 @@
 %
 % function utility = sum_utility(data, labels, train_ind, score_function)
 %
-% where
+% inputs:
 %             data: an (n x d) matrix of input data
 %           labels: an (n x 1) vector of labels
 %        train_ind: a list of indices into data/labels indicating the
