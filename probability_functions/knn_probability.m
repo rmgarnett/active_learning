bb683ab0a8efff2f19c0fa7c3204e84e97566b55
rmgarnett <romanempire@gmail.com>
2012-07-13 00:17:16 -0400

    responses -> labels

--- probability_functions/knn_probability.m
+++ probability_functions/knn_probability.m
@@ -1,15 +1,15 @@
 % binary k-nearest-neighbor classifier.
 %
-% function probabilities = knn_probability(responses, train_ind, ...
-%           test_ind, weights, pseudocount)
+% function probabilities = knn_probability(labels, train_ind, test_ind, ...
+%           weights, pseudocount)
 %
 % inputs:
 %          data: an (n x d) matrix of input data
-%     responses: an (n x 1) vector of responses (class 1 is tested
+%        labels: an (n x 1) vector of labels (class 1 is tested
 %                against "any other class")
-%     train_ind: a list of indices into data/responses indicating
+%     train_ind: a list of indices into data/labels indicating
 %                the training points
-%      test_ind: a list of indices into data/responses indicating
+%      test_ind: a list of indices into data/labels indicating
 %                the test points
 %       weights: an (n x n) matrix of weights
 %   pseudocount: a value in [0, 1] to use as a "pseudocount"
@@ -21,17 +21,17 @@
 %
 % copyright (c) roman garnett, 2011--2012
 
-function probabilities = knn_probability(responses, train_ind, ...
-          test_ind, weights, pseudocount)
+function probabilities = knn_probability(labels, train_ind, test_ind, ...
+          weights, pseudocount)
 
-  % transform responses to handle multi-class
-  responses(responses ~= 1) = 0;
+  % transform labels to handle multi-class
+  labels(labels ~= 1) = 0;
 
   this_weights = weights(test_ind, train_ind);
   total_weight = sum(this_weights, 2);
 
   probabilities = ...
-      (pseudocount + this_weights * responses(train_ind)) ./ ...
+      (pseudocount + this_weights * labels(train_ind)) ./ ...
                 (1 + total_weight);
 
   probabilities = [probabilities, (1 - probabilities)];
