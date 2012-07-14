cd5ad6de12db15bd9bc86d6c6333531a667e631a
rmgarnett <romanempire@gmail.com>
2012-07-13 23:12:30 -0400

    no longer needed

--- implementations/graph_tools/extract_upper_triangle_vector.m
+++
@@ -1,6 +0,0 @@
-function upper_triangle_vector = extract_upper_triangle_vector(matrix)
-
-  upper_triangle = triu(matrix, 1);
-  upper_triangle_vector = upper_triangle(triu(true(size(matrix)), 1));
-
-end
--- implementations/graph_tools/perform_row_normalization.m
+++
@@ -1,5 +0,0 @@
-function A = perform_row_normalization(A)
-
-  A = bsxfun(@times, 1 ./ sum(A, 2), A);
-
-end
