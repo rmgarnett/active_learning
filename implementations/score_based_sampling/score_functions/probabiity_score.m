83b9d5f0b0adef96c919e56507a0333eb592aa09
Roman Garnett <romanempire@gmail.com>
2012-08-23 21:29:05 -0400

    this was silly

--- implementations/score_based_sampling/score_functions/probabiity_score.m
+++
@@ -1,8 +0,0 @@
-function scores = probabiity_score(data, labels, train_ind, ...
-          probability_function)
-
-  test_ind = identity_selector(labels, train_ind);
-  scores = probability_function(data, labels, train_ind, test_ind);
-  scores = scores(:, 1);
-  
-end
