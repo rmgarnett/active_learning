699c15f9a7588140d60eb6646c60c41db9bb2631
Roman Garnett <romanempire@gmail.com>
2012-08-23 21:28:41 -0400

    changing name of entropy_score

--- implementations/uncertainty_sampling/entropy_score.m
+++
@@ -1,7 +0,0 @@
-function scores = entropy_score(data, labels, train_ind, probability_function)
-
-  test_ind = identity_selector(labels, train_ind);
-  probabilities = probability_function(data, labels, train_ind, test_ind);
-  scores = -sum(probabilities .* log(probabilities), 2);
-  
-end
