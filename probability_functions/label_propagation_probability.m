3056c4b13f236da50529cff5780aed24f461f0a3
Roman Garnett <romanempire@gmail.com>
2013-01-03 23:29:37 +0100

    overhaul with inputparser

--- probability_functions/label_propagation_probability.m
+++ probability_functions/label_propagation_probability.m
@@ -1,27 +1,64 @@
 % copyright (c) roman garnett, 2011--2012
 
-function probabilities = label_propagation_probability(A, node_labels, ...
-          train_ind, test_ind, num_iterations)
+function probabilities = label_propagation_probability(A, labels, ...
+          train_ind, test_ind, num_iterations, varargin)
 
-  num_points  = size(A, 1);
+  options = inputParser;
+
+  options.addParamValue('use_prior', false, ...
+                        @(x) (islogical(x) && (numel(x) == 1)));
+  options.addParamValue('pseudocount', 0.1, ...
+                        @(x) (isscalar(x) && (x > 0)));
+  options.addParamValue('store_intermediate', false, ...
+                        @(x) (islogical(x) && (numel(x) == 1)));
+
+  options.parse(varargin{:});
+  options = options.Results;
+
+  num_nodes   = size(A, 1);
+  num_classes = max(labels);
   num_train   = numel(train_ind);
-  num_classes = max(node_labels);
 
-  % preallocate the rows in the label probability matrix for the
-  % labeled nodes
-  train_rows = zeros(num_train, num_classes);
-  for i = 1:numel(train_ind)
-    ind = train_ind(i);
-    train_rows(i, node_labels(ind)) = 1;
+  if (options.use_prior)
+    prior = options.pseudocount * ones(1, num_classes) + ...
+            histc(labels(train_ind), 1:num_classes)';
+    prior = prior / sum(prior);
+  else
+    prior = ones(1, num_classes) / num_classes;
   end
 
-  probabilities = repmat(mean(train_rows), [num_points, 1]);
+  train_rows = accumarray([(1:num_train)', labels(train_ind)], 1, ...
+                          [num_train, num_classes]);
 
-  for i = 1:num_iterations;
+  current_probabilities = repmat(prior, [num_nodes, 1]);
+
+  if (options.store_intermediate)
+    probabilities = zeros(num_nodes, num_classes, num_iterations + 1);
+  else
+    probabilities = zeros(num_nodes, num_classes);
+  end
+
+  iteration = 0;
+  while (true)
     % "pull-back" known labels
-    probabilities(train_ind, :) = train_rows;
-    probabilities = A * probabilities;
+    current_probabilities(train_ind, :) = train_rows;
+
+    if (options.store_intermediate)
+      probabilities(:, :, iteration + 1) = current_probabilities;
+    end
+
+    if (iteration == num_iterations)
+      break;
+    end
+
+    current_probabilities = A * current_probabilities;
+
+    iteration = iteration + 1;
   end
 
-  probabilities = probabilities(test_ind, :);
+  if (options.store_intermediate)
+    probabilities = probabilities(test_ind, :, :);
+  else
+    probabilities = current_probabilities(test_ind, :);
+  end
 end
