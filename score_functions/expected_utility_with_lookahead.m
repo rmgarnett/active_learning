107374f71d34639ca0607707ace86ba546e23fee
Roman Garnett <romanempire@gmail.com>
2014-01-20 19:08:59 +0100

    updating implementation and documentation

--- score_functions/expected_utility_with_lookahead.m
+++ score_functions/expected_utility_with_lookahead.m
@@ -1,13 +1,14 @@
-% finds the optimal next point to add to a dataset for active learning
-% on a set of discrete points for a particular utility function and
-% lookahead.  this function supports using user-defined:
+% Calculates the k-step expected utilities of adding each of a
+% given set of points to a dataset for active learning on a set
+% of discrete points for a particular utility function and
+% lookahead.  This function supports using user-defined:
 %
 % - utility functions, which calculate the utility of a selected set
 %   of points,
-% - probability functions, which assign probabilities to indicated
-%   test data from the current training set, and
+% - models, which assign probabilities to indicated test data from the
+%   current training set, and
 % - selection functions, which specify which among the unlabeled
-%   points should have their expected utilities evaluated. this
+%   points should have their expected utilities evaluated. This
 %   implementation allows multiple selection functions to be used,
 %   should different ones be desired for different lookaheads.
 %
@@ -29,9 +30,9 @@
 %              lookahead: the number of steps to look ahead. if
 %                         lookahead = 0, a random point is selected.
 %
-% outputs:
-%   best_utility: the expected utility of the best point found
-%       best_ind: the index of the best point found
+% output:
+%   expected_utilities: the lookahead-step expected utilities for the
+%                       points in test_ind
 %
 % copyright (c) roman garnett, 2011--2012
 
@@ -44,17 +45,19 @@ function expected_utilities = ...
 
   % if lookahead = 0, pick a random point
   if (lookahead == 0)
-    best_utility = inf;
-    best_ind = test_ind(randi(num_test));
+    expected_utilities = rand(num_test, 1);
+    return;
+
+  % for one-step lookahead, return values from base expected utility
+  elseif (lookahead == 1)
+    expected_utilities = expected_utility(problem, train_ind, ...
+            observed_labels, test_ind);
     return;
   end
 
-  % calculate the current posterior probabilities
-  probabilities = probability(problem, train_ind, observed_labels, test_ind);
-
-  % we will calculate the expected utility of adding each dataset to the
+  % We will calculate the expected utility of adding each dataset to the
   % training set by sampling over labels to create ficticious datasets
-  % and measuring the expected utility of each.  we accomplish
+  % and measuring the expected utility of each. We accomplish
   % lookahead by calling this function recursively to calculate the
   % expected utility at later levels.
   if (lookahead == 1)
@@ -63,35 +66,20 @@ function expected_utilities = ...
     return;
   else
 
-    % used to recursively select test points.  allow array of selector and fall
-    % back if no entry for current lookahead.
-    selector = selectors{min(max(1, lookahead), numel(selectors))};
+  % Used to recursively select test points. Allow array of selector and
+  % fall back if no entry for current lookahead.
+  selector = selectors{min(lookahead - 1, numel(selectors))};
 
-    fake_observed_labels    = [observed_labels; nan];
-    fake_train_ind          = [train_ind; nan];
-    fake_expected_utilities = zeros(problem.num_classes, 1);
+  % Given one additional new point, we will always select the remaining
+  % points by maximizing the (lookahead - 1) expected utility.
+  lookahead_utility = @(problem, train_ind, observed_labels) ...
+      max(expected_utility_with_lookahead(problem, train_ind, ...
+          observed_labels, selector(problem, train_ind, observed_labels), ...
+          probability, expected_utility, selectors, lookahead - 1));
 
-    expected_utilities = zeros(num_test, 1);
-    for i = 1:num_test
-      fake_train_ind(end) = test_ind(i);
-
-      % sample over labels
-      for fake_label = 1:problem.num_classes
-        fake_observed_labels(end) = fake_label;
-
-        fake_test_ind = selector(problem, fake_train_ind, fake_observed_labels);
-
-        % recursive call
-        fake_utilities(fake_label) = ...
-            expected_utility_with_lookahead(problem, fake_train_ind, fake_observed_labels, ...
-                                            fake_test_ind, probability, expected_utility, ...
-                                            selectors, lookahead - 1);
-      end
-
-      % calculate expectation using current probabilities
-      expected_utilities(i) = probabilities(i, :) * fake_utilities;
-    end
-
-  end
+  % Use expected_utility_naive to evaluate the expected utility of the
+  % given points.
+  expected_utilities = expected_utility_naive(problem, train_ind, ...
+          observed_labels, test_ind, probability, lookahead_utility);
 
 end
