e932e290db61c0ba490a3f499e83251198b0c1e8
Roman Garnett <romanempire@gmail.com>
2014-01-20 16:40:35 +0100

    renaming identified_test_set_selector to fixed_test_set_selector

---
+++ selection_functions/fixed_test_set_selector.m
@@ -0,0 +1,38 @@
+% A selector that selects all points besides those in an identified
+% test set.
+%
+% function test_ind = fixed_test_set_selector(problem, train_ind, ...
+%           observed_labels, test_set_ind)
+%
+% inputs:
+%    problem: a struct describing the problem, containing the field:
+%
+%       points: an (n x d) data matrix for the avilable points
+%
+%         train_ind: a list of indices into problem.points indicating
+%                    the thus-far observed points
+%
+%                    Note: this input is ignored by fixed_test_set_selector.
+%                    If desired, it can be replaced by an empty matrix.
+%
+%   observed_labels: a list of labels corresponding to the
+%                    observations in train_ind
+%
+%                    Note: this input is ignored by fixed_test_set_selector.
+%                    If desired, it can be replaced by an empty matrix.
+%
+%      test_set_ind: a list of indicies into problem.points
+%                    indicating the test set
+%
+% output:
+%    test_ind: a list of indices into problem.points indicating the
+%              points to consider for labeling
+%
+% Copyright (c) Roman Garnett, 2011--2014
+
+function test_ind = fixed_test_set_selector(problem, ~, ~, test_set_ind)
+
+  test_ind = identity_selector(problem, [], []);
+  test_ind(test_set_ind) = [];
+
+end
