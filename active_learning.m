2a57ae6021532d34866dfd08d54bdfaa03b4c4a9
Roman Garnett <romanempire@gmail.com>
2014-01-20 17:37:11 +0100

    adding callback, changing messages, updating documenation

--- active_learning.m
+++ active_learning.m
@@ -1,33 +1,41 @@
-% performs active learning on a set of discrete points using a given
+% Performs active learning on a set of discrete points using a given
 % query strategy.
 %
 % function [chosen_ind, chosen_labels] = ...
-%       active_learning(problem, train_ind, observed_labels, query_strategy)
+%       active_learning(problem, train_ind, observed_labels, label_oracle, ...
+%                       selector, query_strategy, callback)
 %
 % inputs:
 %           problem: a struct describing the problem, containing fields:
 %
-%                   points: an n x d matrix describing the avilable points
-%              num_classes: the number of classes
-%              num_queries: the number of queries to make
-%                  verbose: whether to print messages after each
-%                           query (default: false)
+%                  points: an (n x d) data matrix for the available points
+%             num_classes: the number of classes
+%             num_queries: the number of queries to make
+%                 verbose: whether to print information regarding
+%                          each query (default: false)
 %
-%         train_ind: a (possibly empty) list of indices
-%                    indicating the starting labeled points
+%         train_ind: a (possibly empty) list of indices indicating
+%                    the labeled points at start
 %   observed_labels: a (possibly empty) list of labels corresponding
 %                    to the observations in train_ind
-%      label_oracle: a function taking an index into problem.points
-%                    and returning a label
+%      label_oracle: a handle to a label oracle. which takes an index
+%                    into problem.points and returns a label
 %          selector: a handle to a point selector, which specifies
-%                     which points are eligable to query at a given time
+%                    which points are eligible to query at a given time
 %    query_strategy: a handle to a query strategy
+%          callback: (optional) a handle to an arbitrary user-defined
+%                    callback called after each new point is queried.
+%                    The callback will be called as
+%
+%                      callback(problem, train_ind, observed_labels)
+%
+%                    and anything returned will be ignored.
 %
 % outputs:
 %      chosen_ind: a list of indices of the chosen datapoints, in order
 %   chosen_labels: a list of the corresponding observed labels
 %
-% copyright (c) roman garnett, 2011--2013
+% Copyright (c) Roman Garnett 2011--2014
 
 function [chosen_ind, chosen_labels] = ...
       active_learning(problem, train_ind, observed_labels, label_oracle, ...
@@ -42,25 +50,34 @@ function [chosen_ind, chosen_labels] = ...
   for i = 1:problem.num_queries
     if (verbose)
       tic;
-      fprintf('point %i: ', i);
+      fprintf('point %i:', i);
     end
 
+    % get list of points to consider for querying this round
     test_ind = selector(problem, train_ind, observed_labels);
-
-    % find the optimal next point to add given the current training set
-    % and chosen utility function
-    query_ind = query_strategy(problem, train_ind, observed_labels, ...
-                               test_ind);
-
-    % add the selected point and measure our current success
-    chosen_ind(i)    = query_ind;
-    train_ind        = [train_ind; chosen_ind(i)];
-    chosen_labels(i) = label_oracle(problem, chosen_ind(i));
-    observed_labels  = [observed_labels; chosen_labels(i)];
-
     if (verbose)
-      elapsed = toc;
-      fprintf('done, took: %.2fs.\n', elapsed);
+      fprintf(' %i points for consideration', numel(test_ind));
+    end
+
+    % select location of next observation from the given list
+    chosen_ind(i) = query_strategy(problem, train_ind, observed_labels, test_ind);
+
+    % observe label at chosen location
+    chosen_labels(i) = label_oracle(problem, chosen_ind(i));
+
+    % update lists with new observation
+    train_ind       = [train_ind; chosen_ind(i)];
+    observed_labels = [observed_labels; chosen_labels(i)];
+    if (verbose)
+      fprintf(', done, point %i chosen (label: %i), took: %.2fs.\n', ...
+              chosen_ind(i),    ...
+              chosen_labels(i), ...
+              toc);
+    end
+
+    % call callback, if defined
+    if (nargin > 6)
+      callback(problem, train_ind, observed_labels);
     end
   end
 
