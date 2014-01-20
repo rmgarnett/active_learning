3eaa8ac69369d2ff9b8255b4ada19c4962e587aa
Roman Garnett <romanempire@gmail.com>
2014-01-20 17:30:32 +0100

    updating documentation and api

--- label_oracles/lookup_oracle.m
+++ label_oracles/lookup_oracle.m
@@ -1,7 +1,31 @@
-% trivial lookup table oracle given a list of fixed ground truth
-% labels
+% A trivial lookup-table label oracle. Given a query point, returns
+% the corresponding label fromn a given list of fixed ground truth
+% labels.
+%
+% function label = lookup_oracle(problem, query_ind, labels)
+%
+% inputs:
+%     problem: a struct describing the problem, containing the fields:
+%
+%            points: an (n x d) data matrix for the available points
+%       num_classes: the number of classes
+%
+%              Note: the first input is ignored by this function. If
+%              desired, it can be replaced with an empty matrix. The
+%              struct is documented only for reference below.
+%
+%   query_ind: an index into problem.points specifying the point to be
+%              queried
+%      labels: a length-n vector of ground-truth class labels for
+%              each point in problem.points
+%
+% output:
+%     label: an integer between 1 and problem.num_classes indicating
+%            the observed label
+%
+% Copyright (c) Roman Garnett 2013--2014
 
-function label = lookup_oracle(query_ind, labels)
+function label = lookup_oracle(~, query_ind, labels)
 
   label = labels(query_ind);
 
