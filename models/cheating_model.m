57bea177c5ad3fdea509ea670504918a127e1b71
Roman Garnett <romanempire@gmail.com>
2014-03-02 18:13:27 +0100

    documentation

--- models/cheating_model.m
+++ models/cheating_model.m
@@ -1,42 +1,56 @@
-% A "cheating" model.
+% CHEATING_MODEL a "cheating" model that queries a label oracle.
 %
 % This model always predicts a delta distribution for each test point
-% with mass on the output of a given label oracle.
+% with mass on the output of a given label oracle. This can be useful
+% for comparing against a theoretically optimal algorithm.
 %
-% function probabilities = cheating_model(problem, train_ind, ...
-%           observed_labels, test_ind, label_oracle)
+% Usage:
 %
-% inputs:
+%   probabilities = cheating_model(problem, train_ind, observed_labels, ...
+%           test_ind, label_oracle)
+%
+% Inputs:
+%
 %           problem: a struct describing the problem, which must at
 %                    least contain the field:
 %
 %             num_classes: the number of classes
 %
+%                    as well as any fields that may be required by
+%                    the label oracle below.
+%
 %         train_ind: a list of indices into problem.points indicating
 %                    the thus-far observed points
 %
-%                    Note: this input is ignored by cheating_model.
-%                    If desired, it can be replaced by an empty matrix.
+%                    Note: this input, part of the standard
+%                    probability model API, is ignored by
+%                    cheating_model. If desired, for standalone use it
+%                    can be replaced by an empty matrix.
 %
 %   observed_labels: a list of labels corresponding to the
 %                    observations in train_ind
 %
-%                    Note: this input is ignored by cheating_model.
-%                    If desired, it can be replaced by an empty matrix.
+%                    Note: this input, part of the standard
+%                    probability model API, is ignored by
+%                    cheating_model. If desired, for standalone use it
+%                    can be replaced by an empty matrix.
 %
 %          test_ind: a list of indices into problem.points indicating
 %                    the test points
 %      label_oracle: a handle to a label oracle. which takes an index
 %                    into problem.points and returns a label
 %
-% outputs:
+% Outputs:
+%
 %   probabilities: a matrix of posterior probabilities. The ith column
 %                  gives the posterior probabilities p(y = i | x, D)
 %                  for each of the indicated test points; here
 %                  p(y = i | x, D) = 1 if the label oracle output i
 %                  for y; otherwise 0.
 %
-% See also models.
+% See also MODELS, LABEL_ORACLES.
+
+% Copyright (c) 2012--2014 Roman Garnett.
 
 function probabilities = cheating_model(problem, ~, ~, test_ind, label_oracle)
 
@@ -48,5 +62,3 @@ function probabilities = cheating_model(problem, ~, ~, test_ind, label_oracle)
   end
 
 end
-
-% Copyright (c) Roman Garnett, 2012--2014
