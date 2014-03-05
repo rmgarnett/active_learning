6a0d52fd299ebc4eb29b7abf4d537ccda9dc5a9f
rmgarnett <romanempire@gmail.com>
2014-03-05 20:59:52 +0100

    reimplemented as expected_01_loss

--- score_functions/utilities/expected_accuracy.m
+++
@@ -1,7 +0,0 @@
-function expected_utility = expected_accuracy(problem, train_ind, ...
-          observed_labels, test_ind, model)
-
-  probabilities = model(problem, train_ind, observed_labels, test_ind);
-  expected_utility = mean(max(probabilities, [], 2));
-
-end
