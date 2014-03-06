820aeb184330cabbb2336f16ac8ba3e7c630f8a8
rmgarnett <romanempire@gmail.com>
2014-03-06 01:55:59 +0100

    documentation

--- score_functions/margin.m
+++ score_functions/margin.m
@@ -28,13 +28,12 @@
 %   observed_labels: a list of labels corresponding to the
 %                    observations in train_ind
 %          test_ind: a list of indices into problem.points indicating
-%                    the test points
-%             model: a function handle to a probability model
+%                    the points eligible for observation
+%             model: a handle to a probability model
 %
 % Output:
 %
-%   scores: a vector of marginal entropies for each point specified by
-%           test_ind.
+%   scores: a vector of margins for each point specified by test_ind
 %
 % See also: SCORE_FUNCTIONS, MODELS, MARGIN_SAMPLING.
 
--- score_functions/marginal_entropy.m
+++ score_functions/marginal_entropy.m
@@ -1,9 +1,9 @@
 % MARGINAL_ENTROPY calculates predictive entropy on given test points.
 %
-% The predictive marginal entropy H for a point x given observations D
-% is given by
+% The predictive marginal entropy H for a point x given a set of
+% observations D = (X, Y) is given by
 %
-%   H[y | x, D] = -\sum_i p(y = i | x, D) \log(p(y = i | x, D)).
+%   H[y | x, D] = -\sum_i p(y = i | x, D) \log p(y = i | x, D).
 %
 % Maximizing the marginal entropy gives rise to a common query
 % strategy known as uncertainty sampling.
@@ -25,13 +25,13 @@
 %   observed_labels: a list of labels corresponding to the
 %                    observations in train_ind
 %          test_ind: a list of indices into problem.points indicating
-%                    the test points
-%             model: a function handle to a probability model
+%                    the points eligible for observation
+%             model: a handle to a probability model
 %
 % Output:
 %
 %   scores: a vector of marginal entropies for each point specified by
-%           test_ind.
+%           test_ind
 %
 % See also: SCORE_FUNCTIONS, MODELS, UNCERTAINTY_SAMPLING.
 
@@ -41,6 +41,10 @@ function scores = marginal_entropy(problem, train_ind, observed_labels, ...
           test_ind, model)
 
   probabilities = model(problem, train_ind, observed_labels, test_ind);
+
+  % remove any zeros from probabilities to approximate 0 * -inf = 0
+  probabilities = max(probabilities, 1e-100);
+
   scores = -sum(probabilities .* log(probabilities), 2);
 
 end
