6051524206f0f38c9c6e05f19dc7411c400f1452
rmgarnett <romanempire@gmail.com>
2014-03-06 01:56:12 +0100

    documentation

--- score_functions/score_functions.m
+++ score_functions/score_functions.m
@@ -1,23 +1,42 @@
-% Score function should have the following interface:
+% A score function computes an arbitrary score for each given test
+% point that is in some way related to its influence or suitability
+% for making an observation there. Score functions are typically
+% converted into query strategies by either maximization (e.g. argmax)
+% or minimization (e.g. argmin) over the points eligible for
+% observation.
+%
+% Score function must satisfy the following interface:
 %
 %   scores = score_function(problem, train_ind, observed_labels, test_ind)
 %
 % Inputs:
+%
 %           problem: a struct describing the problem, containing fields:
 %
 %                  points: an (n x d) data matrix for the available points
 %             num_classes: the number of classes
-%             num_queries: the number of queries to make
 %
 %         train_ind: a list of indices into problem.points indicating
 %                    the thus-far observed points
 %   observed_labels: a list of labels corresponding to the
 %                    observations in train_ind
 %          test_ind: a list of indices into problem.points indicating
-%                    the test points
+%                    the points eligible for observation
 %
 % Output:
+%
 %   scores: a vector of real-valued scores; one for each point
 %           specified by test_ind
+%
+% The following score functions are provided in this toolbox:
+%
+%   expected_loss_lookahead: multiple-step lookahead expected loss
+%                            arbitrary loss functions
+%       expected_loss_naive: one-step lookahead expected loss for
+%                            arbitrary loss functions
+%                    margin: the predictive margin
+%          marginal_entropy: the predictive entropy
+%
+% See also ARGMIN, ARGMAX.
 
-% Copyright (C) Roman Garnett, 2014
+% Copyright (c) 2014 Roman Garnett.
