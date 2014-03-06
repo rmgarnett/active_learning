fbc47c1465f255b0b93125ab063fa178a8484ac6
rmgarnett <romanempire@gmail.com>
2014-03-06 01:55:08 +0100

    renaming utilities to losses

--- score_functions/expected_loss_lookahead.m
+++ score_functions/expected_loss_lookahead.m
@@ -1,120 +1,122 @@
-% EXPECTED_UTILITY_LOOKAHEAD calculates "lookahed" expected utilities.
+% EXPECTED_LOSS_LOOKAHEAD calculates "lookahed" expected losses.
 %
 % This is an implementation of a score function that calculates the
-% k-step-lookahead expected utilities of adding each of a given set of
-% points to a dataset for a particular utility function and
-% lookahead horizon k.
+% k-step-lookahead expected losses after adding each of a given set of
+% points to a dataset for a particular loss function and lookahead
+% horizon k.
 %
-% This function supports using user-defined:
+% This function supports user-specified:
 %
-% - _Utility functions,_ which calculate the utility of a selected set
-%   of points,
-% - _Models,_ which assign probabilities to indicated test data from
-%   the current training set, and
-% - _Selectors_ which specify which among the unlabeled points should
-%   have their expected utilities evaluated. This implementation
-%   allows multiple selectors to be used, should different ones be
-%   desired for different lookaheads.
+% * _Loss functions,_ which calculate the loss associated with a
+%   selected training set,
 %
-% Note on utility functions:
+% * _Selectors,_ which given the current training set, specify which
+%   points should have their expected losses evaluated. This
+%   implementation allows multiple selectors to be used, should
+%   different ones be desired for different lookaheads.
 %
-% This function requires as an input a function, expected_utility,
-% that will return the one-step-lookahead expected utilities of adding
-% each of a given set of points to a dataset for the chosen utility
-% function. That is, given a utility function u(D) for a dataset D =
-% (X, Y) and a point x, this function should return
+% Note on Expected Loss Functions:
 %
-%   E_y[ u(D U {(x, y)}) | x, D] = ...
-%                   \sum_{i = 1}^D p(y = i | x, D) u(D U {(x, i)})
+% This function requires as an input a function, expected_loss, that
+% will return the one-step-lookahead expected losses after adding each
+% of a given set of points to a dataset for the chosen loss
+% function. That is, given a point x and a loss function \ell(D) for a
+% dataset D = (X, Y), this function should return
 %
-% The API for this expected utility function is the same as for any
+%   E_y[ \ell(D U {(x, y)}) | x, D] = ...
+%                       \sum_i p(y = i | x, D) \ell(D U {(x, i)}),
+%
+% where i ranges over the possible labels.
+%
+% The API for this expected loss function is the same as for any
 % score function:
 %
-%   expected_utilities = expected_utility(problem, train_ind, ...
-%           observed_labels, test_ind)
+%   expected_losses = expected_loss(problem, train_ind, observed_labels, ...
+%           test_ind)
 %
 % Sometimes this expectation over y may be calculated directly without
 % enumerating all cases for y. If that is not possible, the function
-% expected_utility_naive may be used with any arbitrary utility
-% function to calculate this expectation naively (by augmenting the
-% dataset D with (x, i) for each class i and weigting the resulting
-% utilities by the probabiliy that y = i).
-%%
+% expected_loss_naive may be used with any arbitrary loss function to
+% calculate this expectation naively (by augmenting the dataset D with
+% (x, i) for each class i and weigting the resulting losses by the
+% probabiliy that y = i).
+%
 % Usage:
 %
-%   expected_utilities = expected_utility_lookahead(problem, train_ind, ...
-%           observed_labels, test_ind, model, expected_utility, ...
-%           selectors, lookahead)
+%   expected_losses = expected_loss_lookahead(problem, train_ind, ...
+%           observed_labels, test_ind, model, expected_loss, selectors, ...
+%           lookahead)
 %
 % Inputs:
 %
-%            problem: a struct describing the problem, containing fields:
+%           problem: a struct describing the problem, containing fields:
 %
-%                   points: an n x d matrix describing the avilable points
-%              num_classes: the number of classes
-%              num_queries: the number of queries to make
+%                  points: an n x d matrix describing the avilable points
+%             num_classes: the number of classes
 %
-%          train_ind: a list of indices into problem.points
-%                     indicating the thus-far observed points
-%    observed_labels: a list of labels corresponding to the
-%                     observations in train_ind
-%              model: a handle to probability model to use
-%   expected_utility: a handle to the one-step expected utility
-%                     function to use (see note above)
-%          selectors: a cell array of selectors to use. If lookahead == k,
-%                     then the min(k, numel(selection_functions))th
-%                     element of this array will be used.
-%          lookahead: the number of steps to look ahead. If
-%                     lookahead == 0, then random expected utilities
-%                     are returned.
+%         train_ind: a list of indices into problem.points
+%                    indicating the thus-far observed points
+%   observed_labels: a list of labels corresponding to the
+%                    observations in train_ind
+%          test_ind: a list of indices into problem.points indicating
+%                    the points eligible for observation
+%             model: a handle to probability model to use
+%     expected_loss: a handle to the one-step expected loss function
+%                    to use (see note above)
+%         selectors: a cell array of selectors to use. If lookahead == k,
+%                    then the min(k, numel(selection_functions))th
+%                    element of this array will be used.
+%         lookahead: the number of steps to look ahead. If
+%                    lookahead == 0, then random expected losses are
+%                    returned.
 %
 % Output:
 %
-%   expected_utilities: the lookahead-step expected utilities for the
-%                       points in test_ind
+%   expected_loss: the lookahead-step expected losses for the points
+%                  in test_ind
 %
-% See also UTILITIES, EXPECTED_UTILITY_NAIVE, SELECTORS, MODELS, SCORE_FUNCTIONS.
+% See also LOSS_FUNCTIONS, EXPECTED_LOSS_NAIVE, SELECTORS, MODELS, SCORE_FUNCTIONS.
 
 % Copyright (c) 2011--2014 Roman Garnett.
 
-function expected_utilities = expected_utility_lookahead(problem, ...
-          train_ind, observed_labels, test_ind, model, expected_utility, ...
-          selectors, lookahead)
+function expected_losses = expected_loss_lookahead(problem, train_ind, ...
+          observed_labels, test_ind, model, expected_loss, selectors, ...
+          lookahead)
 
   num_test = numel(test_ind);
 
-  % for zero-step lookahead, return random expected utilities
+  % for zero-step lookahead, return random expected losses
   if (lookahead == 0)
-    expected_utilities = rand(num_test, 1);
+    expected_losses = rand(num_test, 1);
     return;
 
-  % for one-step lookahead, return values from base expected utility
+  % for one-step lookahead, return values from base expected loss
   elseif (lookahead == 1)
-    expected_utilities = expected_utility(problem, train_ind, ...
-            observed_labels, test_ind);
+    expected_losses = expected_loss(problem, train_ind, observed_labels, ...
+            test_ind);
     return;
   end
 
-  % We will calculate the expected utility of adding each dataset to the
+  % We will calculate the expected loss after adding each dataset to the
   % training set by sampling over labels to create ficticious datasets
-  % and measuring the expected utility of each. We accomplish
-  % lookahead by calling this function recursively to calculate the
-  % expected utility at later levels.
+  % and measuring the expected loss of each. We accomplish lookahead
+  % by calling this function recursively to calculate the expected
+  % loss at later levels.
 
-  % Used to recursively select test points. Allow array of selector and
+  % Used to recursively select test points. Allow array of selectors and
   % fall back if no entry for current lookahead.
   selector = selectors{min(lookahead - 1, numel(selectors))};
 
   % Given one additional new point, we will always select the remaining
-  % points by maximizing the (lookahead - 1) expected utility.
-  lookahead_utility = @(problem, train_ind, observed_labels) ...
-      max(expected_utility_lookahead(problem, train_ind, observed_labels, ...
+  % points by minimizing the (lookahead - 1) expected loss.
+  lookahead_loss = @(problem, train_ind, observed_labels) ...
+      min(expected_loss_lookahead(problem, train_ind, observed_labels, ...
           selector(problem, train_ind, observed_labels), model, ...
-          expected_utility, selectors, lookahead - 1));
+          expected_loss, selectors, lookahead - 1));
 
-  % Use expected_utility_naive to evaluate the expected utility of the
-  % given points.
-  expected_utilities = expected_utility_naive(problem, train_ind, ...
-          observed_labels, test_ind, model, lookahead_utility);
+  % Use expected_loss_naive to evaluate the expected loss of the given
+  % points.
+  expected_losses = expected_loss_naive(problem, train_ind, ...
+          observed_labels, test_ind, model, lookahead_loss);
 
 end
--- score_functions/expected_loss_naive.m
+++ score_functions/expected_loss_naive.m
@@ -1,25 +1,74 @@
-function expected_utilities = expected_utility_naive(problem, ...
-          train_ind, observed_labels, test_ind, model, utility)
+% EXPECTED_LOSS_NAIVE calculates one-step-lookahead expected losses.
+%
+% This is an implementation of a score function that calculates the
+% one-step-lookahead expected losses after adding each of a given set of
+% points to a dataset for a particular loss function.
+%
+% Given a loss function \ell(D), this function computes the
+% expected loss after adding each identified point x to the current
+% dataset D:
+%
+%   E_y[ \ell(D U {(x, y)}) | x, D] = ...
+%                       \sum_i p(y = i | x, D) \ell(D U {(x, i)}),
+%
+% where i ranges over the possible labels.
+%
+% Here this expectation is computed naively by augmenting the dataset
+% D with (x, i) for each class i and weigting the resulting losses by
+% the probabiliy that y = i.
+%
+% Usage:
+%
+%   expected_losses = expected_loss_naive(problem, train_ind, ...
+%           observed_labels, test_ind, model, loss)
+%
+% Inputs:
+%
+%           problem: a struct describing the problem, containing fields:
+%
+%                  points: an n x d matrix describing the avilable points
+%             num_classes: the number of classes
+%
+%         train_ind: a list of indices into problem.points
+%                    indicating the thus-far observed points
+%   observed_labels: a list of labels corresponding to the
+%                    observations in train_ind
+%          test_ind: a list of indices into problem.points indicating
+%                    the points eligible for observation
+%             model: a handle to probability model to use
+%              loss: a handle to the loss function to use
+%
+% Output:
+%
+%   expected_loss: the lookahead-step expected losses for the points
+%                  in test_ind
+%
+% See also LOSS_FUNCITONS, EXPECTED_LOSS_LOOKAHEAD, MODELS, SCORE_FUNCTIONS.
+
+% Copyright (c) 2011--2014 Roman Garnett.
+
+function expected_losses = expected_loss_naive(problem, train_ind, ...
+          observed_labels, test_ind, model, loss)
 
   num_test = numel(test_ind);
 
   % calculate the current posterior probabilities
   probabilities = model(problem, train_ind, observed_labels, test_ind);
 
-  expected_utilities = zeros(num_test, 1);
-  parfor i = 1:num_test
+  expected_losses = zeros(num_test, 1);
+  for i = 1:num_test
     fake_train_ind = [train_ind; test_ind(i)];
 
     % sample over labels
-    fake_utilities = zeros(problem.num_classes, 1);
+    fake_losses = zeros(problem.num_classes, 1);
     for fake_label = 1:problem.num_classes
       fake_observed_labels = [observed_labels; fake_label];
-      fake_utilities(fake_label) = ...
-          utility(problem, fake_train_ind, fake_observed_labels);
+      fake_losses(fake_label) = ...
+          loss(problem, fake_train_ind, fake_observed_labels);
     end
 
     % calculate expectation using current probabilities
-    expected_utilities(i) = probabilities(i, :) * fake_utilities;
+    expected_losses(i) = probabilities(i, :) * fake_losses;
   end
 
 end
